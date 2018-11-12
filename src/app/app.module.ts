c24f0ff9e97f16f1ed9da0c1fbcc7fcb226987c7
Lanre Adelowo <yo@lanre.wtf>
2018-11-12 01:37:25 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FeedFormComponent } from "./feed-form/feed-form.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: "new", component: FeedFormComponent },
+  {
+    path: "",
+    redirectTo: "/dashboard",
+    pathMatch: "full"
+  }
+  {path: "dashboard", component: DashboardComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FeedFormComponent, PageNotFoundComponent, DashboardComponent],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
